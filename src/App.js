59b3f4594e8e213da334a6281ec886ee968933d8
MikeNotu <mikebateman98@gmail.com>
2021-11-15 00:57:04 -0600

    Added code to test result

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const renderContent = () => (
+    <>
+      <div className="Parallax__content__heading">
+        <h1 className="Parallax__content__heading__text">Closure</h1>
+        <h2 className="Parallax__content__heading__caption">
+          Your one-stop source of Web Development tricks
+        </h2>
+      </div>
+      <div className="Parallax__content__cta">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>1. Like the video.</b> Because it helps me and my channel
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <b>2. Like the video.</b> To see more content like that!
+        </p>
+        <p>
+          <b>3. Follow the Github link.</b> And play with this code yourself!
+        </p>
+      </div>
+    </>
+  );
+
+  return (
+    <section className="Parallax">
+      <div
+        className="Parallax__background"
+        style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+      />
+      <div
+        className="Parallax__background-triangles"
+        style={{ transform: `translateY(${offsetY * 0.8}px)` }}
+      />
+      <div className="Parallax__content">{renderContent()}</div>
+    </section>
   );
 }
 
